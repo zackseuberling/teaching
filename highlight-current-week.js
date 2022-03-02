f6b73c0c9325177dacaa31a0cff0811426332fcc
Zack Seuberling <seuberling@gmail.com>
2022-03-02 15:05:17 -0800

    fix: week highlighting

--- highlight-current-week.js
+++ highlight-current-week.js
@@ -1,12 +1,16 @@
 const date = new Date();
-const today = `${date.getFullYear()}${date.getMonth()}${date.getDate()}`;
-console.log(today);
+const today = Intl.DateTimeFormat("en-GB").format(date);
+const regex = /(\d{2})\/(\d{2})\/(\d{4})/;
+
+const todayFormatted = parseInt(today.replace(regex, "$3$2$1"), 10);
 
 const elements = document.querySelectorAll(".week");
 const weeks = Array.from(elements).map((element) => element.dataset.jsDate);
 
 const currentWeek = weeks.reduce((acc, week) => {
-  return Math.abs(week - today) < Math.abs(acc - today) ? week : acc;
+  return Math.abs(week - todayFormatted) < Math.abs(acc - todayFormatted)
+    ? week
+    : acc;
 });
 
 document
